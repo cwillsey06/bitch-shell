641213c54c21bac3a6db306f4d6a453a094cf0d9
Coltrane Willsey <56744718+cwillsey06@users.noreply.github.com>
2022-03-16 13:42:59 -0400

    add help to Profile

--- src/shell/Profile.lua
+++ src/shell/Profile.lua
@@ -2,10 +2,28 @@
 -- Coltrane Willsey
 -- 2022-03-07 [22:47]
 
+local stdout = require('src/common/stdout')
 local task = require('src/common/task')
 local pack = require('src/common/pack')
 
-local sh_profile = {
+local sh_profile = {}
+sh_profile = {
+    help = {
+        meta = 'echos all commands or $arg1 meta to console';
+        task = task.new(function(self,...)
+            if {...}[1] then
+                return {...}[1] .." : ".. sh_profile[{...}[1]].meta
+            else
+                local t = {}
+                local i = 1
+                for k, cmd in pairs(sh_profile) do
+                    t[i] = ("%s : %s"):format(tostring(i), k) 
+                    i = i + 1
+                end
+                return t
+            end
+        end)
+    };
     echo = {
         meta = 'echos $args to console';
         task = task.new(function(self, ...)
